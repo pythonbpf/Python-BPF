3abe07c5b27bb689b6a0fadfa8d81ca16dd3c32c
varun-r-mallya <varunrmallya@gmail.com>
2025-10-05 14:02:46 +0530

    add global symbol table populate function

--- tests/failing_tests/undeclared_values.py
+++ tests/failing_tests/undeclared_values.py
@@ -8,7 +8,7 @@ from ctypes import c_void_p, c_int64
 @section("tracepoint/syscalls/sys_enter_execve")
 def sometag(ctx: c_void_p) -> c_int64:
     print("test")
-    print(f"{somevalue}")
+    print(f"{somevalue}")  #type: ignore
     return c_int64(1)
 
 @bpf
