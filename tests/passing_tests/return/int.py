3e68d6df4f757dedd5269112a0024a6ae92c8ec5
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-06 04:57:04 +0530

    Add passing test examples for return statements

---
+++ tests/passing_tests/return/int.py
@@ -0,0 +1,18 @@
+from pythonbpf import bpf, section, bpfglobal, compile
+from ctypes import c_void_p, c_int64
+
+
+@bpf
+@section("tracepoint/syscalls/sys_enter_execve")
+def hello_world(ctx: c_void_p) -> c_int64:
+    print("Hello, World!")
+    return 1
+
+
+@bpf
+@bpfglobal
+def LICENSE() -> str:
+    return "GPL"
+
+
+compile()
