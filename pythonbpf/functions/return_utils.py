192e03aa98908679e24526f77345aa7c8a343d01
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-05 23:59:04 +0530

    Add _handle_typed_constant_return

--- pythonbpf/functions/return_utils.py
+++ pythonbpf/functions/return_utils.py
@@ -2,6 +2,7 @@ import logging
 import ast
 
 from llvmlite import ir
+from pythonbpf.type_deducer import ctypes_to_ir
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -18,6 +19,23 @@ def _handle_none_return(builder) -> bool:
     """Handle return or return None -> returns 0."""
     builder.ret(ir.Constant(ir.IntType(64), 0))
     logger.debug("Generated default return: 0")
+    return True
+
+
+def _handle_typed_constant_return(call_type, return_value, builder, ret_type) -> bool:
+    """Handle typed constant return like: return c_int64(42)"""
+
+    # call_type = stmt.value.func.id
+    expected_type = ctypes_to_ir(call_type)
+
+    if expected_type != ret_type:
+        raise ValueError(
+            f"Return type mismatch: expected {ret_type}, got {expected_type}"
+        )
+
+    # return_value = stmt.value.args[0].value
+    builder.ret(ir.Constant(ret_type, return_value))
+    logger.debug(f"Generated typed constant return: {call_type}({return_value})")
     return True
 
 
