1cce49f5e01db66aca9c59913db71dc4dbabc001
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-07 03:24:11 +0530

    Add const_binop test for conditionals

---
+++ tests/passing_tests/conditionals/const_binop.py
@@ -0,0 +1,21 @@
+from pythonbpf import bpf, section, bpfglobal, compile
+from ctypes import c_void_p, c_int64
+
+
+@bpf
+@section("tracepoint/syscalls/sys_enter_execve")
+def hello_world(ctx: c_void_p) -> c_int64:
+    if (0 + 1) * 0:
+        print("Hello, World!")
+    else:
+        print("Goodbye, World!")
+    return
+
+
+@bpf
+@bpfglobal
+def LICENSE() -> str:
+    return "GPL"
+
+
+compile()
