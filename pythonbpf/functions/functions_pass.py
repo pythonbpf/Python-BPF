aae7aa981d6cb96915bdaa1a966c6a0ac834132b
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-08 06:49:34 +0530

    Fix expr imports

--- pythonbpf/functions/functions_pass.py
+++ pythonbpf/functions/functions_pass.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from pythonbpf.helper import HelperHandlerRegistry, handle_helper_call
 from pythonbpf.type_deducer import ctypes_to_ir
 from pythonbpf.binary_ops import handle_binary_op
-from pythonbpf.expr_pass import eval_expr, handle_expr, convert_to_bool
+from pythonbpf.expr import eval_expr, handle_expr, convert_to_bool
 
 from .return_utils import _handle_none_return, _handle_xdp_return, _is_xdp_name
 
