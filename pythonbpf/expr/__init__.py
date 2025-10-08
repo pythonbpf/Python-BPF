6362a5e665026b0380efa4be548b3ce7b2959b49
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2025-10-08 07:24:14 +0530

    Fix expr imports

--- pythonbpf/expr/__init__.py
+++ pythonbpf/expr/__init__.py
@@ -1,3 +1,4 @@
-from .expr_pass import eval_expr, handle_expr, convert_to_bool
+from .expr_pass import eval_expr, handle_expr
+from .type_normalization import convert_to_bool
 
 __all__ = ["eval_expr", "handle_expr", "convert_to_bool"]
