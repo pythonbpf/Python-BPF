11e8e721886d7c1379ec61a308117a55ec2b7719
varun-r-mallya <varunrmallya@gmail.com>
2025-10-15 02:00:23 +0530

    add base for ir gen

--- pythonbpf/vmlinux_parser/ir_gen/ir_generation.py
+++ pythonbpf/vmlinux_parser/ir_gen/ir_generation.py
@@ -8,10 +8,28 @@ class IRGenerator:
     def __init__(self, module, handler: DependencyHandler):
         self.module = module
         self.handler: DependencyHandler = handler
+        self.generated: list[str] = []
         if not handler.is_ready:
             raise ImportError(
                 "Semantic analysis of vmlinux imports failed. Cannot generate IR"
             )
         for struct in handler:
-            print(struct)
+            self.struct_processor(struct)
             print()
+
+    def struct_processor(self, struct):
+        if struct.name not in self.generated:
+            print(f"IR generating for {struct.name}")
+            print(f"Struct is {struct}")
+            for dependency in struct.depends_on:
+                if dependency not in self.generated:
+                    dep_node_from_dependency = self.handler[dependency]
+                    self.struct_processor(dep_node_from_dependency)
+                    self.generated.append(dependency)
+            # write actual processor logic here after assuming all dependencies are resolved
+            # this part cannot yet resolve circular dependencies. Gets stuck on an infinite loop during that.
+            self.generated.append(struct.name)
+
+
+    def struct_name_generator(self, ):
+        pass
