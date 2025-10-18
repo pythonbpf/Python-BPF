de19c8fc9008b4d9cd05abde646097e53ad483c1
varun-r-mallya <varunrmallya@gmail.com>
2025-10-18 22:15:58 +0530

    rename passing test

--- tests/passing_tests/vmlinux/simple_struct_test.py
+++ tests/passing_tests/vmlinux/simple_struct_test.py
@@ -6,9 +6,9 @@ from ctypes import c_int64
 
 # Instructions to how to run this program
 # 1. Install PythonBPF: pip install pythonbpf
-# 2. Run the program: python examples/xdp_pass.py
-# 3. Run the program with sudo: sudo tools/check.sh run examples/xdp_pass.o
-# 4. Attach object file to any network device with something like ./check.sh xdp examples/xdp_pass.o tailscale0
+# 2. Run the program: python examples/simple_struct_test.py
+# 3. Run the program with sudo: sudo tools/check.sh run examples/simple_struct_test.o
+# 4. Attach object file to any network device with something like ./check.sh run examples/simple_struct_test.o tailscale0
 # 5. send traffic through the device and observe effects
 @bpf
 @section("tracepoint/syscalls/sys_enter_execve")
@@ -23,5 +23,5 @@ def LICENSE() -> str:
     return "GPL"
 
 
-compile_to_ir("xdp_pass.py", "xdp_pass.ll")
+compile_to_ir("simple_struct_test.py", "simple_struct_test.ll")
 compile()
