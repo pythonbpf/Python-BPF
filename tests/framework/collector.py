56db0d3738a257e52126275f662b886050041637
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2026-04-25 17:59:59 +0530

    Tests: Remove unnecessary version check from test collector

--- tests/framework/collector.py
+++ tests/framework/collector.py
@@ -1,10 +1,6 @@
-import sys
 from pathlib import Path
 
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
+import tomllib
 
 from .bpf_test_case import BpfTestCase
 
